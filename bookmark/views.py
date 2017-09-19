67d08cd801f9bd216645f0b211800970c7bb615e
KimHK <srchr@hanmail.net>
2017-09-19 20:00:17 +0900

    urls.py update

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DateDetailView
 # Create your views here.
+from .models import Bookmark
+
+class BookmarkListV(ListView):
+    model = Bookmark
+
+class BookmarkDetailV(DateDetailView):
+    model = Bookmark
+
+
